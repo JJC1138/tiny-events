5f012bdc1819a0b8816767eba50c404db8b8045e
Jon Colverson <github@vcxz.co.uk>
2017-06-02 01:08:49 +0100

    Tests: Added more test cases

--- Tests/TinyEventsTests/TinyEventsTests.swift
+++ Tests/TinyEventsTests/TinyEventsTests.swift
@@ -30,9 +30,83 @@ class TinyEventsTests: XCTestCase {
         XCTAssertEqual(data, dataFromObserver)
     }
     
+    func testFiringRepeatedly() {
+        let event = TinyEvent()
+        
+        let timesToFire = 26
+        var timesObserved = 0
+        
+        withExtendedLifetime(event.add({ timesObserved += 1 })) {
+            for _ in 1...timesToFire {
+                event.fire()
+            }
+        }
+        
+        XCTAssertEqual(timesToFire, timesObserved)
+    }
+    
+    func testRemovingObserver() {
+        let event = TinyEvent()
+        
+        var timesObserved = 0
+        
+        var observer: TinyEventObserver? = event.add { timesObserved += 1 }
+        
+        withExtendedLifetime(observer) {
+            event.fire()
+        }
+        
+        XCTAssertEqual(timesObserved, 1)
+        
+        observer = nil
+        
+        event.fire()
+        
+        XCTAssertEqual(timesObserved, 1)
+    }
+    
+    func testMultipleObservers() {
+        let event = TinyEvent()
+        
+        var timesObserved = 0
+        let observerCount = 33
+        
+        var observers = [TinyEventObserver]()
+        
+        for _ in 1...observerCount {
+            observers.append(event.add { timesObserved += 1 })
+        }
+        
+        event.fire()
+        
+        XCTAssertEqual(observerCount, timesObserved)
+    }
+    
+    func testWithMultiplePiecesOfData() {
+        let event = TinyEventWithData<(String, Int)>()
+        
+        var titleFromObserver: String? = nil
+        var ratingFromObserver: Int? = nil
+        
+        let observer = event.add { title, rating in
+            titleFromObserver = title
+            ratingFromObserver = rating
+        }
+        withExtendedLifetime(observer) {
+            event.fire(("TinyEvents", Int.max))
+        }
+        
+        XCTAssertNotNil(titleFromObserver)
+        XCTAssertNotNil(ratingFromObserver)
+    }
+    
     static var allTests = [
         ("testSimple", testSimple),
         ("testWithIntData", testWithIntData),
+        ("testFiringRepeatedly", testFiringRepeatedly),
+        ("testRemovingObserver", testRemovingObserver),
+        ("testMultipleObservers", testMultipleObservers),
+        ("testWithMultiplePiecesOfData", testWithMultiplePiecesOfData),
     ]
     
 }
