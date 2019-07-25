36936a2c3faa9155ee9636f84f984a299eaa208a
Jon Colverson <github@vcxz.co.uk>
2019-07-25 13:58:08 +0100

    Updated Swift Package Manager package description to version 4.0, because 3.1 is no longer supported by the current (Swift 5.1) tools

--- Package.swift
+++ Package.swift
@@ -1,7 +1,12 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
-    name: "TinyEvents"
+    name: "TinyEvents",
+    products: [.library(name: "TinyEvents", targets: ["TinyEvents"])],
+    targets: [
+        .target(name: "TinyEvents"),
+        .testTarget(name: "TinyEventsTests", dependencies: ["TinyEvents"]),
+    ]
 )
